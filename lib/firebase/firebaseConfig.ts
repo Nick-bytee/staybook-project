6fddfd4a408a8ecc381eb23bcf6b59d658aaf1ca
Nick-bytee <81418302+Nick-bytee@users.noreply.github.com>
2024-07-03 17:38:12 +0530

    Update firebaseConfig.ts

--- lib/firebase/firebaseConfig.ts
+++ lib/firebase/firebaseConfig.ts
@@ -3,12 +3,7 @@ import { getFirestore } from "firebase/firestore";
 
 // copy your app config from the firebase
 const firebaseConfig = {
-  apiKey: "AIzaSyBvLhqGITh9x6Ex0xSoILSR23WB46BrsvM",
-  authDomain: "temp-f2858.firebaseapp.com",
-  projectId: "temp-f2858",
-  storageBucket: "temp-f2858.appspot.com",
-  messagingSenderId: "480587411387",
-  appId: "1:480587411387:web:719e55532d59fa9dc29e9e"
+
 };
 
 // Initialize Firebase
